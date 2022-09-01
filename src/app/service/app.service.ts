51cddffe17bc79008fcb61cf5b5bd5da48da4746
Adilbencheikh <111605287+Adilbencheikh@users.noreply.github.com>
2022-09-01 13:20:05 +0100

    probleme de js non detecté

---
+++ src/app/service/app.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AppService{
+
+    constructor(private http : HttpClient){
+
+    }
+
+    sendEmail() : void{
+       // this.http.
+    }
+}
